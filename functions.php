edc0bedb96304d03a7db856e35ad5397d84159a9
Fauzan <fauzanlubis23@gmail.com>
2023-02-20 10:06:24 +0800

    add page

--- functions.php
+++ functions.php
@@ -2,10 +2,11 @@
 
 function idisumut_files() {
     wp_enqueue_style('main-stylesheet', get_theme_file_uri('/pages/css/pages.css'));
-    wp_enqueue_style('main-pace', get_theme_file_uri('assets/plugins/pace/pace-theme-flash.css'));
-    wp_enqueue_style('main-bootstrap', get_theme_file_uri('assets/plugins/bootstrap/css/bootstrap.min.css'));
-    wp_enqueue_style('main-fontawesome', get_theme_file_uri('assets/plugins/font-awesome/css/font-awesome.css'));
-    wp_enqueue_style('main-swiper', get_theme_file_uri('assets/plugins/swiper/css/swiper.css'));
+    wp_enqueue_style('main-icon', get_theme_file_uri('/pages/css/pages-icons.css'));
+    wp_enqueue_style('main-pace', get_theme_file_uri('/assets/plugins/pace/pace-theme-flash.css'));
+    wp_enqueue_style('main-bootstrap', get_theme_file_uri('/assets/plugins/bootstrap/css/bootstrap.min.css'));
+    wp_enqueue_style('main-fontawesome', get_theme_file_uri('/assets/plugins/font-awesome/css/font-awesome.css'));
+    wp_enqueue_style('main-swiper', get_theme_file_uri('/assets/plugins/swiper/css/swiper.css'));
 }
 
 add_action('wp_enqueue_scripts', 'idisumut_files');
--- page.php
+++ page.php
@@ -1,7 +1,28 @@
 <?php
+get_header();
     while(have_posts()) {
         the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+
+          <!-- BEGIN HERO WITH PARALLAX -->
+    <section class="demo-hero-5" data-pages="parallax" style="background-image: url(<?php echo get_theme_file_uri('assets/images/gallery_hero_1.jpg') ?>)">
+      <div class="container-xs-height full-height">
+        <div class="col-xs-height col-middle text-center">
+          <h1 class="text-white inner"><?php the_title(); ?></h1>
+        </div>
+      </div>
+    </section>
+    <!-- END HERO WITH PARALLAX -->
+    <!-- BEGIN PORTFOLIO SECTION -->
+    <section class="p-b-40 p-t-40">
+      <div class="container">
+        <div class="m-b-30">
+          <h1 class="text-center ">Showcase.</h1>
+          <div class="b-b b-grey m-t-30"></div>
+            <?php the_content(); ?>
+        </div>
+      </div>
+       
 <?php }
+
+get_footer();
 ?>
