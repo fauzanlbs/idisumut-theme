8b19933925fdcc6a330f4968bdefcdea880ed639
Fauzan <fauzanlubis23@gmail.com>
2023-02-21 10:02:47 +0800

    update title

--- page.php
+++ page.php
@@ -7,7 +7,7 @@ get_header();
     <section class="demo-hero-5" data-pages="parallax" style="background-image: url(<?php echo get_theme_file_uri('assets/images/doctor-bg2.jpg') ?>);height: 100%;background-size: cover;background-position: center;background-repeat: no-repeat;">
       <div class="container-xs-height full-height">
         <div class="col-xs-height col-middle text-center">
-          <h1 class="text-white inner"></h1>
+          <h1 style="font-size:50px" class="block-title text-black inner"><b><?php the_title(); ?></b></h1>
         </div>
       </div>
     </section>
@@ -16,7 +16,7 @@ get_header();
     <section class="p-b-40 p-t-40">
       <div class="container">
         <div class="m-b-30">
-          <h1 class="text-center "><?php the_title(); ?></h1>
+          <h1 class="text-center "></h1>
           <div class="b-b b-grey m-t-30"></div>
             <?php the_content(); ?>
         </div>
