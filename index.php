1c3bd056f8efc68dfc05ed540e8bf3e057809eb8
Fauzan <fauzanlubis23@gmail.com>
2023-02-26 01:13:07 +0800

    fix same name post type with page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ get_header(); ?>
  <section class="demo-hero-5" data-pages="parallax" style="background-image: url(<?php echo get_theme_file_uri('assets/images/doctor-bg2.jpg') ?>);height: 100%;background-size: cover;background-position: center;background-repeat: no-repeat;">
       <div class="container-xs-height full-height">
         <div class="col-xs-height col-middle text-center">
-          <h1 style="font-size:50px" class="block-title text-black inner"><b>WELCOME</b></h1>
+          <h1 style="font-size:50px" class="block-title text-black inner"><b></b></h1>
         </div>
       </div>
     </section>
