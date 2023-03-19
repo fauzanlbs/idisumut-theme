d55cdc47c57a8c1e4135119b987b63d2117361a6
Fauzan <fauzanlubis23@gmail.com>
2023-03-19 19:00:50 +0800

    remove big picture detail page

--- single.php
+++ single.php
@@ -3,13 +3,7 @@ get_header();
     while(have_posts()) {
         the_post(); ?>
            <!-- BEGIN HERO WITH PARALLAX -->
-    <section class="demo-hero-5" data-pages="parallax" style="background-image: url(<?php echo get_theme_file_uri('assets/images/doctor-bg2.jpg') ?>);height: 100%;background-size: cover;background-position: center;background-repeat: no-repeat;">
-      <div class="container-xs-height full-height">
-        <div class="col-xs-height col-middle text-center">
-          <h1 style="font-size:50px" class="block-title text-black inner"><b><?php the_title(); ?></b></h1>
-        </div>
-      </div>
-    </section>
+   
     <!-- END HERO WITH PARALLAX -->
     <!-- BEGIN PORTFOLIO SECTION -->
     <section class="p-b-40 p-t-40">
